8164d829e03ede366cc16f2470708963985f7dfa
Mark-Kitur <kimutai.workspace@gmail.com>
2024-10-20 07:56:42 +0300

    final

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, render_template
 import numpy as np
 import joblib
@@ -6,54 +7,62 @@ import joblib
 model_dia = joblib.load("ray.joblib")
 model_hype = joblib.load("hgbc_model_for_hypertension.joblib")
 
+# Initialize Flask app
 app = Flask(__name__, template_folder='templete')
 
 # Home route
 @app.route('/')
 def home():
     return render_template('home.html')
-
+prediction=None
+@app.route('/reco')
+def reco():
+    return render_template('text.html')
 # Diabetes prediction route
 @app.route('/diabetes', methods=['GET', 'POST'])
 def Diabetes():
+    global prediction
     if request.method == 'POST':
         features = [float(x) for x in request.form.values()]
         # Create an array for the diabetes model
-        array = np.array(features).reshape(1,-1)
+        array = np.array(features).reshape(1, -1)
         
         # Make prediction
         pred = model_dia.predict(array)
         
         if pred == 0.0:
-            prediction = "Diabetic"
+            prediction = "High blood sugar"
         elif pred == 1.0:
-            prediction = 'Non-Diabetic'
+            prediction = 'Low blood sugar'
         else:
-            prediction = "Pre-Diabetic"
+            prediction = "Normal blood sugar"
         
-        return render_template('index.html', prediction=prediction)
+        return render_template('index.html', prediction=prediction )
     
     return render_template('index.html')
 
 
-# Hypertension prediction route
 @app.route('/hypertension', methods=['GET', 'POST'])
 def Hypertension():
+    global response
     if request.method == 'POST':
         features = [float(xx) for xx in request.form.values()]
+        array = np.array(features).reshape(1, -1)
         
-        array = np.array(features).reshape(1,-1)
-        
+        # Make prediction
         pred = model_hype.predict(array)
+        
         if pred == 0.0:
             prediction = "High Blood Pressure"
-       
+ 
         else:
             prediction = "Low Blood Pressure"
+        
         
         return render_template('hpe.html', prediction=prediction)
     
     return render_template('hpe.html')
 
+    
 if __name__ == "__main__":
     app.run(debug=True)
